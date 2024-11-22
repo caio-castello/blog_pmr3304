f7c6a9126e3a0a7a40e0058ab9ba7077d169e466
Caio Castello Grandi Ribeiro <caiocastello@usp.br>
2024-11-21 23:59:17 -0300

    Alterar labels do forms para pt-br

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -5,3 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+        labels = {
+            'title': 'Título',
+            'content': 'Conteúdo',
+        }
--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Conteúdo")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
