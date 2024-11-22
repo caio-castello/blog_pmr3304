d2080c28edbc8d65ec38256d241a0ad836d99656
Caio Castello Grandi Ribeiro <caiocastello@usp.br>
2024-11-21 23:20:43 -0300

    Criar views funcionais utilizando Django forms

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -106,12 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-bt'
 
-TIME_ZONE = 'America/Sao Paulo'
-
+TIME_ZONE = 'America/Sao_Paulo'
 USE_L10N = True
-
 USE_I18N = True
-
 USE_TZ = True
 
 
